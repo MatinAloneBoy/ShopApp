7b490197ba026f197c284a3a379b7ab95e5c1655
matin abgineh <matinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.com>
2021-07-12 14:01:46 +0430

    First Version

---
+++ main/java/com/example/myapplication/User.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class User {
+    public String Email,Password,UserName;
+    public Double PhoneNum;
+    enum UserMode{
+        Admin,
+        Seller,
+        Customer
+    }
+
+}
