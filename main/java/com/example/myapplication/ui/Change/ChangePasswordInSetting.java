83e5261ffea1688505d45979de435bbb16fc92e7
matin abgineh <matinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.commatinabgineh.aloneboy@gmail.com>
2021-07-17 08:29:34 +0430

    Change Password Setting DataBase Complete.

--- main/java/com/example/myapplication/ui/Change/ChangePasswordInSetting.java
+++ main/java/com/example/myapplication/ui/Change/ChangePasswordInSetting.java
@@ -1,66 +1,56 @@
 package com.example.myapplication.ui.Change;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.example.myapplication.database.UpdateProfileDataBase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChangePasswordInSetting#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jetbrains.annotations.NotNull;
+
 public class ChangePasswordInSetting extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button sendPassButton;
+    private TextView newPassText;
+    private EditText newPassBox;
+    UpdateProfileDataBase upd;
 
-    public ChangePasswordInSetting() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChangePasswordInSetting.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChangePasswordInSetting newInstance(String param1, String param2) {
-        ChangePasswordInSetting fragment = new ChangePasswordInSetting();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
+    @org.jetbrains.annotations.Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_forget_pass,container,false);
+        sendPassButton=view.findViewById(R.id.ChangePasswordPage_button_frame);
+        newPassText=view.findViewById(R.id.new_Password_to_change_text);
+        newPassBox=view.findViewById(R.id.new_Password_to_change_edit_text);
+        String mail;
+        Context context = getActivity();
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                getString(R.string.profile_file_key), Context.MODE_PRIVATE);
+        mail=sharedPref.getString(String.valueOf(R.string.profile_email_key),"");
+        sendPassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Sending Email", Toast.LENGTH_SHORT).show();
+                upd=new UpdateProfileDataBase(getContext());
+                upd.updatePhoneNumber(newPassBox.getText().toString(),mail);
+            }
+        });
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_password_in_setting, container, false);
+        return view;
     }
 }
