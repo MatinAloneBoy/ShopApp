3120a85058d5b16da2b7d46364a5b17cf5a0a332
SURFACE\arian <Arian8Ultra>
2021-07-19 13:35:08 +0430

    login page with new navigations

---
+++ ShopApp/src/main/java/login/ForgetPassFragment.java
@@ -0,0 +1,52 @@
+package com.example.myapplication.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+import com.example.myapplication.database.ForgotPassDataBase;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ForgetPassFragment extends Fragment {
+
+
+    private Button sendEmailButton;
+    private TextView ForgetEmailText,ForgotPassText;
+    private EditText ForgetEmailBox,ForgotPassbox;
+    ForgotPassDataBase fsd;
+
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_forget_pass,container,false);
+        sendEmailButton=view.findViewById(R.id.forgotten_button);
+        ForgetEmailText=view.findViewById(R.id.forgotten_email_text);
+        ForgetEmailBox=view.findViewById(R.id.forgotten_email_edit_text);
+        ForgotPassText=view.findViewById(R.id.new_password_text);
+        ForgotPassbox=view.findViewById(R.id.new_password_edit_text);
+
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Sending Email", Toast.LENGTH_SHORT).show();
+                fsd=new ForgotPassDataBase(getContext());
+                fsd.updateCourses(ForgotPassbox.getText().toString(),ForgetEmailBox.getText().toString());
+            }
+        });
+
+        return view;
+    }
+}
---
+++ ShopApp/src/main/java/login/LoginFragment.java
@@ -0,0 +1,186 @@
+package com.example.myapplication.login;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.myapplication.userUi.home.HomeBottomActivity;
+import com.example.myapplication.R;
+import com.example.myapplication.database.UsersDataBase;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LoginFragment extends Fragment {
+
+
+    private Button login;
+    private EditText email_edit_text,password_edit_text;
+    private TextView forgetPass,error_text,register_text;
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    UsersDataBase us;
+
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(intent);
+                        handleSignInResult(task);
+                    }
+                }
+            });
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_login,container,false);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        login =view.findViewById(R.id.login);
+        email_edit_text=view.findViewById(R.id.edit_text_email);
+        password_edit_text=view.findViewById(R.id.edit_text_pass);
+        forgetPass=view.findViewById(R.id.forgot_Text);
+        error_text=view.findViewById(R.id.error_text);
+        signInButton= view.findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_ICON_ONLY);
+        register_text=view.findViewById(R.id.new_account);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email_edit_text.getText().equals("arian8rezaei@gmail.com")){
+                    Navigation.findNavController(v).navigate(R.id.action_loginFragment_to_adminPage);
+                    Toast.makeText(getActivity(),"Hello Admin", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getActivity(),email_edit_text.getText(), Toast.LENGTH_SHORT).show();
+                    Navigation.findNavController(v).navigate(R.id.action_loginFragment_to_adminPage);
+                }
+            }
+        });
+
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        register_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment);
+            }
+        });
+
+        forgetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_forgetPassFragment);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity());
+        updateUI(account);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        mStartForResult.launch(signInIntent);
+        //startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount acct) {
+        if (acct!=null){
+            String personPhoto;
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            if(!acct.getPhotoUrl().equals(null)){
+                personPhoto = acct.getPhotoUrl().toString();
+
+            }else {
+                personPhoto ="https://www.pngkey.com/png/full/157-1579943_tracy-mcgrady-png.png";
+
+            }
+
+///////////////////////////////SharedPreferences
+            Context context = getActivity();
+            SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.profile_file_key), Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(String.valueOf(R.string.profile_name_key),personName);
+            editor.putString(String.valueOf(R.string.profile_email_key),personEmail);
+            editor.putString(String.valueOf(R.string.profile_id_key),personId);
+            editor.putString(String.valueOf(R.string.profile_photo_key),personPhoto);
+            editor.apply();
+            us=new UsersDataBase(getContext());
+            us.register_user(personName,personEmail,personId+personFamilyName,"NormalUser","-",acct.getPhotoUrl().toString());
+            Intent intent= new Intent(getContext(), HomeBottomActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+            Toast.makeText(getActivity(), personName, Toast.LENGTH_SHORT).show();
+
+        }else {
+            Toast.makeText(getActivity(), "Could not sign in with Google", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
